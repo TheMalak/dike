11e65da09c748428d15b2d422bf73cc5975fed8f
TheMalak <david.tejeda.santos@gmail.com>
2024-03-05 18:08:53 -0600

    responsive on components added

--- src/app/shared/components/hero/hero.component.ts
+++ src/app/shared/components/hero/hero.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WindowSizeService } from '../../services/windowsize.service';
 
 @Component({
   selector: 'shared-hero',
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.sass'
 })
-export class HeroComponent {
+export class HeroComponent implements OnInit {
   public backgroungImage: string = 'url("assets/hero.png")';
+  public backgroungImageMobile: string = 'url("assets/mobile-hero.jpeg")';
+  public mobileBackground: boolean = false;
+  constructor(private windowSizeService: WindowSizeService) { }
+
+  ngOnInit(): void {
+    this.windowSizeService.windowWidth$.subscribe((size: number) => {
+      this.mobileBackground = (size < 600) ? false : true;
+    });
+  }
+
 }
