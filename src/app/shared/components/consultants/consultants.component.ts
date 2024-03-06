96cc46499b5605f1084292c4fbf28dcf350778f1
TheMalak <david.tejeda.santos@gmail.com>
2024-03-05 19:11:29 -0600

    responsivo completado

--- src/app/shared/components/consultants/consultants.component.ts
+++ src/app/shared/components/consultants/consultants.component.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { OwlOptions } from 'ngx-owl-carousel-o';
 import { Consultant } from '../../interfaces/consultant';
 
 @Component({
@@ -14,20 +13,7 @@ export class ConsultantsComponent implements OnInit {
   public whatsAppUrl: string = "https://wa.me/+522222124995?text=Hola,%20me%20gustaría%20saber%20más%20sobre%20sus%20servicios";
   public backgroundImage: string = 'linear-gradient(rgba(34, 83, 131, 0.8), rgba(34, 83, 131, 0.8)),url("assets/services.png")';
 
-  public customOptions: OwlOptions = {
-    loop: true,
-    mouseDrag: true,
-    touchDrag: true,
-    pullDrag: true,
-    dots: true,
-    navSpeed: 700,
-    margin: 30,
-    items: 2,
-    nav: false
-  }
-
   constructor(private http: HttpClient) { }
-
 
   ngOnInit() {
     this.http.get<Consultant[]>('/assets/data/consultants.json').subscribe(data => {
